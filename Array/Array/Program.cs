178d24632dda4a575c427226b84dddf7d587b7fd
Roma Zaluzhny <Zaluzhny@rivne.itstep.org>
2020-02-27 14:58:22 +0200

    f

---
+++ Array/Array/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Початковий вклад в банку рівний 1000 грн.Через кожен місяць розмір вкладу збільшується 
+//на Р процентів від наявної суми(Р - дійсне число, 0 < P< 25). Значення Р программа повинна 
+//отримувати від користувача.По даному Р программа повинна визначити, через скільки місяців розмір 
+//вкладу перевищить 1100 грн.та вивести знайдену кількість місяців К(ціле число) та підсумковий розмір вкладу S(дійсне число).  
+
+
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float P;
+            float Vidsotok;
+            int dep =1000;
+            float S = dep;
+            Console.Write("Enter % :\n");
+            P =Convert.ToInt32(Console.ReadLine());
+            //if (P < 0 && P > 25)
+            //{
+                ConsoleColor er= Console.BackgroundColor;
+                Console.WriteLine("ERROR{0}",er); 
+            //}
+            //else
+            //{
+            //    for (int i = 0; i < 12; i++)
+            //    {
+            //        Vidsotok = S * (P / 100);
+            //        S = S + Vidsotok;
+            //        if (S >= 1100)
+            //        {
+            //            Console.Write("Months : {0}\nPidsumok : {1}\n", i, Convert.ToInt32(S));
+            //            break;
+            //        }
+            //    }
+            //}
+            
+        }
+    }
+}
---
+++ String/String/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// . Вводится строка слов. Вывести слова в обратном порядке.   
+namespace String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region
+            //  string s5 = "Printing backwards";
+            //  for (int i = 0; i < s5.Length; i++)
+            //  {
+            //      Console.Write(s5[s5.Length - i - 1]);
+            //  }
+            //  Console.WriteLine("\n");
+            #endregion
+
+            #region
+            //string str = "BOO i am scaring you!";
+            //Console.WriteLine(str);
+            //str =str.Replace("am","was");
+            //Console.WriteLine(str);
+            #endregion
+
+            #region
+            //string str = "BOO i am scaring you!";
+            //Console.WriteLine(str);
+            //string man = Console.ReadLine();
+            //str.IndexOf(man);
+            //str = str.Replace(man, Console.ReadLine());
+            //Console.WriteLine(str);
+            #endregion
+
+            #region
+            //var Matrix = new char[4][];
+            //Matrix[0] = new char[] { 'w', 'o', 'r', 'l' };
+            //Matrix[1] = new char[] { 's', 'e', 'a', 'b' };
+            //Matrix[2] = new char[] { 'l', 'o', 'a', 'd' };
+            //Matrix[3] = new char[] { 'b', 'o', 's', 's' };
+            //string strl=System.String.Empty;
+            //for (int i = 0; i < Matrix.Length; i++)
+            //{
+            //    for (int j = 0; j < Matrix[i].Length; j++)
+            //    {
+            //       Console.Write(Matrix[i][j]+" ");
+            //         strl = Convert.ToString(strl+Matrix[i][j]);
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //Console.WriteLine(strl);
+            //bool cW = strl.Contains(Console.ReadLine());
+            //Console.WriteLine(cW);
+            #endregion
+
+            #region
+            string text = "    I   did    it    ";
+            string[] words = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            string strl = System.String.Empty;
+            for (int i = 0; i < words.Length; i++)
+            { 
+                for (int j = 0; j < words[i].Length; j++)
+                {
+                    Console.Write(words[i][j]);
+                    strl = Convert.ToString(strl + words[i][j]); 
+                    
+                }
+                Console.WriteLine();
+                strl = Convert.ToString(strl +"*");
+      
+            }
+            strl=strl.TrimEnd('*');
+            Console.WriteLine(strl);
+            #endregion
+        }
+    }
+}
